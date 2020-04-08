832560c097f6db138581efcfce528f3bc93e6d36
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-04-07 20:35:25 -0700

    images and carousel fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ one = Project.new(
   sentence: "Web page displaying a variety of cocktail recipies.",
   idea: "This web page is one of the Le Wagon exercises as students are first being introduced to Rails. The background coding is quite simple, but the result is elegant and relatable. After all, who doesn't love cocktails?!",
   tools: "This is the first web page I built the using Ruby on Rails framework. Javascript enables the fun hover effect on each recipe and PostgreSQL powers the database.",
-  # images: "",
+  images: ["https://i.imgur.com/iEHm8zp.png?1", "https://i.imgur.com/L0LNzmr.png?1"],
   codelink: "https://github.com/Opalevanescence/rails-mister-cocktail",
   sitelink: "")
 
@@ -12,7 +12,7 @@ two = Project.new(
   sentence: "Web application that makes organizing your Fringe experience fast, easy, and intuitive.",
   idea: "Every August, Edinburgh plays host to the magnificent Fringe, the world’s largest arts festival and most ticketed event after the Olympics. 1000s of shows are performed around the clock, ranging from comedy to cabaret, circus, theatre, dance and more! You are thoroughly spoilt for choice but the flip side is the Herculean effort that goes into planning your festival. Fringe-goers are known to painstakingly construct monstrous, heavily formatted spreadsheets that scroll for miles with dozens of shortlisted shows and puzzle-pieced schedules! /n MyFringePlanner makes organizing your Fringe experience fast, easy, and intuitive. It takes the pain out of complex organizing for individuals and groups.",
   tools: "This was coded using a Ruby on Rails framework. There is a little bit of AJAX used to load pages the way we want. The acts-as-follower gem is used to create relationships between users who are friends and between users and their favorited shows /n The 'friends' feature is actually still in the creation stages as is the 'Group' feature that will allow users to create shortlists and planners with a group of friends instead of just as an individual.",
-  images: ["https://i.imgur.com/SOWXFLc.png", "https://i.imgur.com/ruRhf3T.png", "https://i.imgur.com/XAFZhQ3.png", "https://i.imgur.com/M9t8ddm.png"],
+  images: ["https://i.imgur.com/HROkcPy.png?1", "https://i.imgur.com/dyfp5ZX.png?1", "https://i.imgur.com/258lmkI.png?1", "https://i.imgur.com/8BXN2Fy.png?1", "https://i.imgur.com/4wajVE3.png?1"],
   codelink: "https://github.com/deelye/myfringeplanner",
   sitelink: "https://www.myfringeplanner.com/")
 
@@ -21,7 +21,7 @@ three = Project.new(
   sentence: "A site for people to 'buy' and 'sell' plants with each other.",
   idea: "The idea was to create a site with similar functionalities to AirBnB, but with a twist to make it interesting and unique. Our unique approach was to make a marketplace for plants instead of rooms and houses. However, we have intentially not added a way for people to actually exchange plants as we have no intention of maintaining this website.",
   tools: "This Ruby on Rails based website is hosted by Heroku and uses Cloudinary to store and manage its images. The site updates in real time as purchases are made or plants are posted. Most of the front end is done using Bootstrap rather than CSS.",
-  # images: ,
+  images: ["https://i.imgur.com/mEmVuo2.png?1", "https://i.imgur.com/CpY5wcD.png?1", "https://i.imgur.com/42Ca7ay.png?1", "https://i.imgur.com/HDODbWM.png?1", "https://i.imgur.com/rZValpX.png?1"],
   codelink: "https://github.com/Opalevanescence/plantopia1",
   sitelink: "https://plantopia1.herokuapp.com")
 
